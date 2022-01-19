d42ded5c8e2026d33510c5d8a746c39d26d23a1a
lori3f6 <lori3f6@gmail.com>
2022-01-19 20:01:44 +0800

    multiline text support

--- src/main/java/land/melon/lab/simplelanguageloader/components/Text.java
+++ src/main/java/land/melon/lab/simplelanguageloader/components/Text.java
@@ -5,49 +5,133 @@ import land.melon.lab.simplelanguageloader.utils.ColorConverter;
 import land.melon.lab.simplelanguageloader.utils.Pair;
 
 import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-public class Text {
+public final class Text {
     public static final TextSerializer gsonSerializer = new TextSerializer();
-    private final String textOriginal;
-    private final String textExpanded;
-    private final String textColored;
+    private final List<String> textOriginal;
+    private final List<String> textExpanded;
+    private final List<String> textColored;
 
-    public Text(String text) {
-        this.textOriginal = text;
-        textExpanded = ColorConverter.convertConvenientColorCode(text);
-        textColored = textExpanded.replace("&", "§").replace("§§", "&");
+    public Text(String... texts) {
+        textOriginal = new ArrayList<>(texts.length);
+        textExpanded = new ArrayList<>(texts.length);
+        textColored = new ArrayList<>(texts.length);
+        Arrays.stream(texts).forEach(this::addTextItem);
     }
 
-    public String original() {
+    private Text(JsonElement jsonElement) {
+        int arraySize;
+        if (jsonElement.isJsonPrimitive()) {
+            arraySize = 1;
+        } else if (jsonElement.isJsonArray()) {
+            arraySize = jsonElement.getAsJsonArray().size();
+        } else {
+            throw new RuntimeException("Invalid text element in your language file, only String and Array<String> are allowed.");
+        }
+
+        textOriginal = new ArrayList<>(arraySize);
+        textExpanded = new ArrayList<>(arraySize);
+        textColored = new ArrayList<>(arraySize);
+
+        if (jsonElement.isJsonPrimitive()) {
+            addTextItem(jsonElement.getAsString());
+        } else {
+            jsonElement.getAsJsonArray().forEach(e -> addTextItem(e.getAsString()));
+        }
+    }
+
+    public static Text of(String... texts) {
+        return new Text(texts);
+    }
+
+    private void addTextItem(String originalString) {
+        textOriginal.add(originalString);
+        var expanded = ColorConverter.convertConvenientColorCode(originalString);
+        textExpanded.add(expanded);
+        textColored.add(expanded.replace("&", "§").replace("§§", "&"));
+    }
+
+    public List<String> originalAsList() {
         return textOriginal;
     }
 
-    public String expanded() {
+    public String original() {
+        StringBuilder texts = new StringBuilder();
+        originalAsList().forEach(text -> texts.append(text).append("\n"));
+        return texts.toString();
+    }
+
+    public List<String> expandedAsList() {
         return textExpanded;
     }
 
-    public String colored() {
+    public String expanded() {
+        StringBuilder texts = new StringBuilder();
+        expandedAsList().forEach(text -> texts.append(text).append("\n"));
+        return texts.toString();
+    }
+
+    public List<String> coloredAsList() {
         return textColored;
+    }
+
+    public String colored() {
+        StringBuilder texts = new StringBuilder();
+        coloredAsList().forEach(text -> texts.append(text).append("\n"));
+        return texts.toString();
+    }
+
+    @SafeVarargs
+    public final List<String> produceAsList(Pair<String, Object>... pairs) {
+        List<String> result = new ArrayList<>(textColored.size());
+        coloredAsList().forEach(text -> {
+            for (var pair : pairs) {
+                result.add(text.replace("{" + pair.key() + "}", pair.value().toString()));
+            }
+        });
+        return result;
     }
 
     @SafeVarargs
     public final String produce(Pair<String, Object>... pairs) {
-        var result = colored();
+        StringBuilder texts = new StringBuilder();
+        for (var text : textColored) {
+            texts.append(text).append("\n");
+        }
+        var result = texts.toString();
         for (var pair : pairs) {
             result = result.replace("{" + pair.key() + "}", pair.value().toString());
         }
         return result;
     }
 
+    @Override
+    public String toString() {
+        return colored();
+    }
+
     private static class TextSerializer implements JsonSerializer<Text>, JsonDeserializer<Text> {
         @Override
-        public JsonElement serialize(Text text, Type type, JsonSerializationContext jsonSerializationContext) {
-            return new JsonPrimitive(text.original());
+        public JsonElement serialize(Text textInstance, Type type, JsonSerializationContext jsonSerializationContext) {
+            if (textInstance.textOriginal.size() == 0) {
+                return new JsonPrimitive("");
+            } else if (textInstance.textOriginal.size() == 1) {
+                return new JsonPrimitive(textInstance.textOriginal.get(0));
+            } else {
+                var textArray = new JsonArray(textInstance.textOriginal.size());
+                for (var text : textInstance.textOriginal) {
+                    textArray.add(text);
+                }
+                return textArray;
+            }
         }
 
         @Override
         public Text deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
-            return new Text(jsonElement.getAsString());
+            return new Text(jsonElement);
         }
     }
 }
